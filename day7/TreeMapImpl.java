fe328478564142be2893fac94290d3a8eeb90817
tanelaamer <tanel3203@gmail.com>
2022-12-07 12:08:15 +0100

    day 7 done

---
+++ day7/TreeMapImpl.java
@@ -0,0 +1,55 @@
+// assumption: the cd actions move to valid dirs (no validation here)
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.util.TreeMap;
+import java.lang.Integer;
+import java.util.Map;
+
+class TreeMapImpl {
+    public static void main(String[] args) {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("./inputs.txt"))) {
+            String line;
+            TreeMap<String, Integer> map = new TreeMap<>();
+            String currentPath = "./root";
+            map.put(currentPath, 0);
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split(" ");
+                if (tokens[0].equals("$")) {
+                    if (tokens[1].equals("cd")) {
+                        if (tokens[2].equals("..")) {
+                            currentPath = currentPath.substring(0, currentPath.lastIndexOf("/"));
+                        } else if (tokens[2].equals("/")) {
+                            currentPath = "./root";
+                        } else {
+                            currentPath = currentPath + "/" + tokens[2];
+                            map.put(currentPath, 0);
+                        }
+                    };
+                } else {
+                    if (!tokens[0].equals("dir")) {
+                        int total = map.get(currentPath);
+                        total = total + Integer.parseInt(tokens[0]);
+                        map.put(currentPath, total);
+                        String parent = currentPath.substring(0, currentPath.lastIndexOf("/"));
+                        int traversals = currentPath.replaceAll("[^/]", "").length()-1;
+                        for (int i = 0; i < traversals; i++) {
+                            int total2 = map.get(parent);
+                            total2 = total2 + Integer.parseInt(tokens[0]);
+                            map.put(parent, total2);
+                            parent = parent.substring(0, parent.lastIndexOf("/"));
+                        }
+                    }
+                }
+            }
+            int fin = 0;
+            for (Map.Entry<String,Integer> entry : map.entrySet()) {
+                if (entry.getValue() <= 100000) fin = fin + entry.getValue();
+            }
+            System.out.println(fin);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
